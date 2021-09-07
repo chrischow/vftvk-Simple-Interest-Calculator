c0ba84d8129805866f8472405641d10f5d985604
Christian Chow <christian.chow.en.yi@gmail.com>
2021-09-07 21:10:25 +0800

    Add functions to compute interest and update rate displayed

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+// Function to compute interest gained and end year
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // Extract variables
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    // Compute interest and end year
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    
+    // Check principal for positive values
+    if (principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    // Display text in span "result"
+    document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + "\</mark\>,\<br\>" +
+        "at an interest rate of \<mark\>" + rate + "%\</mark\>\<br\>" +
+        "You will receive an amount of \<mark\>" + interest + "\</mark\>,\<br\>" +
+        "in the year " + year
 }
-        
+
+// Function to retrieve rate and display it in target span
+function updateRate(){
+    // Get rate value
+    var rateval = document.getElementById("rate").value;
+
+    // Display rate in span "rate_val"
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
