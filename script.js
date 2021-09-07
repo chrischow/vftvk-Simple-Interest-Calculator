434dc0008abf91588c633e367e9d7090d5cf389e
Christian Chow <christian.chow.en.yi@gmail.com>
2021-09-07 21:56:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function compute()
     document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + "\</mark\>,\<br\>" +
         "at an interest rate of \<mark\>" + rate + "%\</mark\>\<br\>" +
         "You will receive an amount of \<mark\>" + interest + "\</mark\>,\<br\>" +
-        "in the year " + year + "."
+        "in the year \<mark\>" + year + "\</mark\>."
 }
 
 // Function to retrieve rate and display it in target span
